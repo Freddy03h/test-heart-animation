cc31b835267d4da6acf608fca3559e8fa871bd65
freddyharris <freddy03h@gmail.com>
2013-03-28 23:43:28 +0100

    sub-menu

--- app/views/split-view.js
+++ app/views/split-view.js
@@ -20,17 +20,39 @@ function(app, template, MenuView) {
       menuRegion: '#menu-main'
     },
     events: {
-      'click .menu-button': 'openMenu',
+      'click #menu-one': 'openMenu',
+      'click #menu-two': 'openSubMenu',
       'click #menu a': 'openMenu'
     },
     initialize : function(e){
       this.menuView = new MenuView();
+      this.has3d = (Modernizr.cssanimations && Modernizr.csstransforms3d);
     },
     onRender: function(){
       this.menuRegion.show(this.menuView);
     },
     openMenu: function(e){
       this.$el.toggleClass('open-menu');
+    },
+    openSubMenu: function(e){
+      var self = this;
+      var submenu = this.$el.find('#sub-menu');
+      $(e.currentTarget).toggleClass('selected');
+      if(submenu.is(":visible")){
+        if(this.has3d){
+          submenu.on('webkitTransitionEnd transitionEnd', function(e){
+            submenu.off('webkitTransitionEnd transitionEnd');
+            submenu.hide();
+          });
+        }else{
+          submenu.hide();
+        }
+      }else{
+        submenu.show();
+      }
+      setTimeout(function(){
+        self.$el.find('#app').toggleClass('open-submenu');
+      },10);
     }
 
   });
